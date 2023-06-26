8dad7e118043e50c0fd15e9b112a4ab658bf6bae
wolfagame <128586842+wolfagame@users.noreply.github.com>
2023-06-26 11:08:23 +0200

    Add files via upload

--- Rapport d'activité .py
+++ Rapport d'activité .py
@@ -11,15 +11,15 @@ import csv
 
 with open("Rapport d'activité.txt", "w" ) as f :
     with open('registre_transmissions.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
+        reader = csv.reader(csvfile, delimiter = ";")
         for row in reader :
-            if ''.join([row[0][18], row[0][19], row[0][20]]) == "587" :
-                f.write("Envoie d'un email à \n")
-                f.write("".join([row[0][0], row[0][1], row[0][2], row[0][3], row[0][4], row[0][5], row[0][6], row[0][7], row[0][8], row[0][9], row[0][10], row[0][11], row[0][12], row[0][13], row[0][14], row[0][15], row[0][16], row[0][17]]))
-            
-            elif ''.join([row[0][18], row[0][19], row[0][20], row[0][21]]) == "8080" : 
-                f.write("Connexion internet à l'adresse : \n")
-                f.write("".join([row[0][50], row[0][51], row[0][52], row[0][53], row[0][54], row[0][55], row[0][56], row[0][57], row[0][58], row[0][59], row[0][60]]))
+            if row[7] == "eth:ethertype:ip:tcp:smtp" :
+                f.write("Envoie d'un email à ")
+                f.write(row[0] + "\n")
+             
+            elif row[1] == "8080" :
+                f.write("Connexion internet à l'adresse : ")
+                f.write(row[4] + "\n")
                 
                 
                 
